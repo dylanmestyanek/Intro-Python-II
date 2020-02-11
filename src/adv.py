9195db5418c298d48c8b1b492f6e205e2df42181
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-02-10 18:08:45 -0600

    Currently printing player's current room and provide input ability to change direction, but does not change anything

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,12 +39,15 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player_one = Player("outside")
 
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+print(player_one)
+room_to_move = input("Select direction: [1] North [2] East [3] South [4] West       [q] Quit")
 #
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
